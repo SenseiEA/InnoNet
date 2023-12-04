5c8f9050a23909c3c06d9ef22931b2ac53f8310a
SenseiEA <134182302+SenseiEA@users.noreply.github.com>
2023-12-04 22:26:03 +0800

    init commit

--- globaljss.js
+++ globaljss.js
@@ -1,5 +1,39 @@
-function show() {
-    //document.getElementById("dThreshold").display ="block";
-    document.getElementById("searchBar").style.display = "block";
+// modal.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch post content
+    fetch('post.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('modalId').innerHTML = data;
+      })
+      .catch(error => console.error('Error fetching post content:', error));
+  });
+  
+  function openPost() {
+    const myModal = new bootstrap.Modal(document.getElementById('modalId'))
+  }
+  
+  function closePost() {
+    document.getElementById('modalId').style.display = 'none';
+  }
+  
+
+//Videos
+var videos = ["../videos/pastel-blue-loop", "../videos/pastel-pink-loop.mp4"];
+var currentVideoIndex = 0;
+
+function fadeInVideo(video) {
+  video.style.opacity = 1;
+  video.addEventListener('ended', function() {
+    fadeOutVideo(video);
+  });
 }
 
+function fadeOutVideo(video) {
+  video.style.opacity = 0;
+  currentVideoIndex = (currentVideoIndex + 1) % videos.length;
+  video.src = videos[currentVideoIndex];
+  video.load();
+  video.play();
+  fadeInVideo(video);
+}
